94e275f3e431cdde16725e571211e9713beb0e94
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2018-07-01 12:36:19 -0600

    añadir js para correo
    agregar id's

---
+++ main.js
@@ -0,0 +1,8 @@
+//recibir datos de submit
+document.getElementById('Formulario').addEventListener('submit',submitform);
+
+function submitform(e){
+    e.preventDefault();
+
+    console.log(123);
+}
